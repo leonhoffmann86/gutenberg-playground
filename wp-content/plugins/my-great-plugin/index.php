0b218308a26d7fdcc7b15f0258732ec69d92b529
Leon Hoffmann <leon.hoffmann@beawwwer.de>
2021-10-28 08:22:29 +0200

    kinda starting out new with more focus and follwoing tutorial by tutorial

---
+++ wp-content/plugins/my-great-plugin/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: My Great Plugin
+ * Plugin URI: https://github.com/ryanwelcher/twitch/tree/stream/sept-17-2021/plugins/my-great-plugin
+ * Description: From Live Coding Session on https://www.youtube.com/watch?v=riqDs7nBMGg
+ * 
+ */
+
+ 
